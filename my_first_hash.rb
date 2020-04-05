f95a383ebb6516f497fa3a0c0b71cefb678cb6f1
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-05 05:22:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ def my_hash(hash)
 
  my_hash = {"key" => "value", "another key" => "another value", "yet another key" => "yet another value", "some other key" => "some other value"}
 end
-
+binding.pry
 my_hash(hash)
 
 def shipping_manifest
