4e1e3be95b93cd3aa5a50bd6849e1e7a7769f73b
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-05 05:16:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
-def my_hash(hash)
+def my_hash(your_hash)
  your_hash = {"key" => "value", "another key" => "another value", "yet another key" => "yet another value", "some other key" => "some other value"}
 end
 
-my_hash(hash)
+my_hash(your_hash)
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
