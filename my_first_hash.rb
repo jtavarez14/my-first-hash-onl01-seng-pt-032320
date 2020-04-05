f1afef1d3476fb4b6f3fbc073df1b0726844b940
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-05 05:16:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash(hash)
- example_hash = {"key" => "value", "another key" => "another value", "yet another key" => "yet another value", "some other key" => "some other value"}
+ your_hash = {"key" => "value", "another key" => "another value", "yet another key" => "yet another value", "some other key" => "some other value"}
 end
 
 my_hash(hash)
